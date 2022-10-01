830712efe40a7f6aa881a505fe104072562fda4a
Navin <navinsuryawanshi159@gmail.com>
2022-10-01 23:01:36 +0530

    add carousel and cards

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <?php include('include/header.php') ?>
 
 <body>
-    <header class="navbar m-0 shadow navbar-expand-lg navbar-light bg-light ">
+    <header class="navbar m-0 shadow navbar-expand-lg navbar-light bg-light stick" style="position: sticky; top: 0; z-index: 999;">
         <div class="container">
             <a class="navbar-brand t" href="#">E-commerce</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,7 +15,7 @@
                         <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-house me-2"></i>Home</a>
                     </li>
                     <li class="nav-item me-3">
-                        <a class="nav-link active" href="#"><i class="fa-solid fa-list me-2"></i>Categories</a>
+                        <a class="nav-link active" href="#category"><i class="fa-solid fa-list me-2"></i>Categories</a>
                     </li>
                     <li class="nav-item me-3">
                         <a class="nav-link active" href="#"><i class="fa-solid fa-cart-shopping me-2"></i>Add to Cart</a>
@@ -60,7 +60,82 @@
         </div>
     </header>
 
-    <?php include 'carousel.php' ?>
+    <?php '' // include 'carousel.php' 
+    ?>
+
+    <section>
+        <div id="carouselId" class="carousel slide" data-bs-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-bs-target="#carouselId" data-bs-slide-to="0" class="active" aria-current="true" aria-label="First slide"></li>
+                <li data-bs-target="#carouselId" data-bs-slide-to="1" aria-label="Second slide"></li>
+                <li data-bs-target="#carouselId" data-bs-slide-to="2" aria-label="Third slide"></li>
+            </ol>
+            <div class="carousel-inner" style="height: 30vh;" role="listbox">
+                <div class="carousel-item active">
+                    <img src="/Images//sunset.jpg" style="height: 50vh;" class="w-100 d-block" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img src="/Images//lake.jpg" style="height: 50vh;" class="w-100 d-block" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img src="/Images//1011-900x500.jpg" style="height: 50vh;" class="w-100 d-block" alt="Third slide">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </section>
+
+    <section id="category" class="container mx-auto col-12">
+        <div class="row justify-content-center">
+            <div class="col-2 m-2 card text-start">
+                <img class="card-img-top p-2" style="width: 150px; height: 200px;" src="/Images//anlg-428-grey-grey-analogue-original-imagyzrh9gdh9dhc.webp" alt="Title">
+                <hr>
+                <div class="card-body">
+                    <h4 class="card-title">Title</h4>
+                    <p class="card-text">Body</p>
+                </div>
+            </div>
+            <div class="col-2 m-2 card text-start">
+                <img class="card-img-top p-2" style="width: 150px; height: 200px;" src="/Images//anlg-428-grey-grey-analogue-original-imagyzrh9gdh9dhc.webp" alt="Title">
+                <hr>
+                <div class="card-body">
+                    <h4 class="card-title">Title</h4>
+                    <p class="card-text">Body</p>
+                </div>
+            </div>
+            <div class="col-2 m-2 card text-start">
+                <img class="card-img-top p-2" style="width: 150px; height: 200px;" src="/Images//anlg-428-grey-grey-analogue-original-imagyzrh9gdh9dhc.webp" alt="Title">
+                <hr>
+                <div class="card-body">
+                    <h4 class="card-title">Title</h4>
+                    <p class="card-text">Body</p>
+                </div>
+            </div>
+            <div class="col-2 m-2 card text-start">
+                <img class="card-img-top p-2" style="width: 150px; height: 200px;" src="/Images//anlg-428-grey-grey-analogue-original-imagyzrh9gdh9dhc.webp" alt="Title">
+                <hr>
+                <div class="card-body">
+                    <h4 class="card-title">Title</h4>
+                    <p class="card-text">Body</p>
+                </div>
+            </div>
+            <div class="col-2 m-2 card text-start">
+                <img class="card-img-top p-2" style="width: 150px; height: 200px;" src="/Images//anlg-428-grey-grey-analogue-original-imagyzrh9gdh9dhc.webp" alt="Title">
+                <hr>
+                <div class="card-body">
+                    <h4 class="card-title">Title</h4>
+                    <p class="card-text">Body</p>
+                </div>
+            </div>
+        </div>
+    </section>
 
     <section>
         <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
